671ea8cb86844fddaedd65284516c909af8e060e
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-02 08:38:23 +0300

    # fix : links wont send in wizard channel + break msg exceed 2000char to multiple msgs#

--- events_bot.py
+++ events_bot.py
@@ -27,9 +27,63 @@ async def on_message(message):
 		ask_bard_task =  bot.loop.create_task( ask_bard(user_query= wizard_ch_msg.content , user_name= wizard_ch_msg.author.display_name) ) #if error replace display_name with name
 		task_response : tuple = await ask_bard_task
 		bot_msg_header = f"***MIGHTY GPTEOUS Answers :man_mage:! *** \n"
-		full_response = bot_msg_header + task_response[0]
-		await message.reply(content= full_response  , mention_author= True)
-		#TODO : ADD REPLY MESSAGES WITh LINKS IF THERE IS AND IMAGES IF THERE IS 
+		links_msg_header = f"\n```ini\n [Sources & links]``` \n"
+		full_response = bot_msg_header + task_response[0] 
+
+		full_resp_len = len(full_response)
+		if full_resp_len >= 2000 : #break bard response   to smaller messages to fit in discord msg
+     
+			bot_msg_header = f"***MIGHTY GPTEOUS Answers :man_mage:! *** `[this msg will be fragmented: exceeds 2000chars]`\n"
+			full_response = bot_msg_header + task_response[0] 
+			full_resp_len = len(full_response) #re-calc
+   
+			needed_msgs = full_resp_len // 2000
+			remain = full_resp_len % 2000
+			if remain != 0 :
+				needed_msgs += 1
+    
+			msg_frag : str
+			end_flag = "\n```ini\n [END OF MSG]```"
+			while needed_msgs != 0 :
+				
+
+				if needed_msgs == 1 and remain != 0 :
+					if remain > len(end_flag) : #there is place to add in flag in same last msg frag
+						msg_frag = full_response[ : ] + "\n```ini\n [END OF MSG]```"
+						await message.reply(content= msg_frag  , mention_author= True)
+						break
+					else :#no place to add end flag send it seperatly in new discord msg
+						msg_frag = full_response[ : ] 
+						await message.reply(content= msg_frag  , mention_author= True)
+						await message.reply(content= end_flag  , mention_author= True)
+						break
+						
+					
+				elif needed_msgs == 1 and remain == 0 : #send end flag in discord msg to indicate end of full bard response 
+					msg_frag = full_response[ : ] 
+					await message.reply(content= msg_frag  , mention_author= True)
+					await message.reply(content= end_flag  , mention_author= True)
+					break
+  
+				else :
+					msg_frag = full_response[ : 1999]
+     
+				await message.reply(content= msg_frag  , mention_author= True)
+				full_response = full_response[2000 : ]
+					
+
+				needed_msgs -= 1
+				
+			
+		else: #all bard response can fit in one discord msg
+			await message.reply(content= full_response  , mention_author= True)
+
+		links_list = list( set(task_response[1])) #remove duplicate links
+		links_list[0] = '\n * '+ links_list[0]
+		final_links = links_msg_header  + '\n * '.join(links_list)
+		await message.reply(content= final_links  , mention_author= True)
+		#(DONE)TODO : ADD REPLY MESSAGES WITH LINKS IF THERE IS 
+  		#TODO : reply with IMAGES IF THERE IS 
   
 	await bot.process_commands(message) #add this to prevent on_message() from blocking bot.command
 	del message
--- main_wizard_bot.py
+++ main_wizard_bot.py
@@ -7,7 +7,6 @@
                           Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]
 """
 #TODO TODO TODO : DONT ADD MORE TASKS! (FINSIH THOSE & FIX BIG BUGS ) THEN => MIGRATE TO PYCORD 
-#TODO : handle if embed exceeds max size or max size of fields i.e.(25) -> ( bot will continue work anyway but tell user that OF happend of paganating)
 #TODO : Documentaion for functions / classes and repo readme.md
 #(DONE)TODO : make bot see prev messages (use session)
 #(DONE)TODO : talk to bot by mention him  
@@ -15,14 +14,17 @@
 #(DONE)TODO :  reply to his message (take the content of replayed message and respond accordingly to it + new message)
 #(DONE)TODO : use SESSION with ASYNC BARD ( contact BardAPI maker or raise issue in their repo)(I was  kinda  wrong)
 #(DONE)TODO : show embedded links in bard answer 
+#(DONE)TODO : show  images in Wizard special channel
+#(DONE)TODO : send msg in parts is it exceeds max size in bot special channel
 
+#TODO : send Embed in parts/pages if it exceeds max size  (6000char) or exceeds max fields (25 field)
 #TODO : show embedded images in bard answer (shows with the links)
-#TODO : save last conversation id (load it in init_bot.py) in text file and add command to start new conv or  default is to continue old 
-#       the issue is that bard.get_answer() itself doesnt take any args put the query prompt (can easly add new param but prefer search more on this first)
 #TODO : wizard bot sqlite DB : design and connect the db with bot code
 #TODO : use google translator API
 #TODO : OOP it more and handle errors!
 #TODO : connect to cdn that has memes / quotes  and set on_time() event to send to chat chill and ask-wizard-channel (duplicated in todo discord channel)
+#TODO : save last conversation id (load it in init_bot.py) in text file and add command to start new conv or  default is to continue old 
+#       the issue is that bard.get_answer() itself doesnt take any args put the query prompt (can easly add new param but prefer search more on this first)
 
 #NOTE : the rest of tasks is on mod's channel 'todo' in my narol's server + (to find all TODO s search via global vs code search )
 #NOTE : #Heartbeating handled automatically (modify from gateway class) 
