146a7892fae765061a54ff0662e0a093a7aadcc2
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-19 16:11:39 +0300

    # update todos in main_wizard_bot.py #

--- main_wizard_bot.py
+++ main_wizard_bot.py
@@ -16,6 +16,7 @@
 #(DONE)TODO : show embedded links in bard answer 
 #(DONE)TODO : show  images in Wizard special channel
 #(DONE)TODO : send msg in parts is it exceeds max size in bot special channel
+#(DONE)TODO : connect to cdn that has memes / quotes  and set on_time() event to send to chat chill and ask-wizard-channel (duplicated in todo discord channel)
 
 
 #TODO : send Embed in parts/pages if it exceeds max size  (6000char) or exceeds max fields (25 field)
@@ -23,7 +24,6 @@
 #TODO : wizard bot sqlite DB : design and connect the db with bot code
 #TODO : use google translator API
 #TODO : OOP it more and handle errors!
-#TODO : connect to cdn that has memes / quotes  and set on_time() event to send to chat chill and ask-wizard-channel (duplicated in todo discord channel)
 #TODO : save last conversation id (load it in init_bot.py) in text file and add command to start new conv or  default is to continue old 
 #TODO : complete bard_key_refresh.py
 #TODO : add read text in audio feature and ability for bot to join voice chat
