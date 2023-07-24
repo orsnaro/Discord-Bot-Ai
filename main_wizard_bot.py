77b9c166dbec0cacef4cb65a399c3222b43c4d34
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-07-24 18:13:54 +0300

    # add msgfragmenter function task to main_wizard_bot.py #"

--- main_wizard_bot.py
+++ main_wizard_bot.py
@@ -18,7 +18,7 @@
 #(DONE)TODO : send msg in parts is it exceeds max size in bot special channel
 #(DONE)TODO : connect to cdn that has memes / quotes  and set on_time() event to send to chat chill and ask-wizard-channel (duplicated in todo discord channel)
 
-
+#TODO : make message fragmenter function for both msg and links msg in utils_bot.py
 #TODO : send Embed in parts/pages if it exceeds max size  (6000char) or exceeds max fields (25 field)
 #TODO : show embedded images in bard answer (shows with the links)
 #TODO : wizard bot sqlite DB : design and connect the db with bot code
