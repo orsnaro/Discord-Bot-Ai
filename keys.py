2df32c11c244b02832dbf41beb1da38c4670d0fb
Omar Rashad <77698890+orsnaro@users.noreply.github.com>
2023-08-20 06:28:55 +0300

    Update keys.py : update path for render host

--- keys.py
+++ keys.py
@@ -6,17 +6,17 @@
                           Title : #
                           Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]
 """
-with open(r"../openai_apiKey.txt" , 'r') as openaifile :
+with open(r"/etc/secrets/openai_apiKey.txt" , 'r') as openaifile :
    openaiAPI_KEY = openaifile.read().strip()
    
-with open(r"../discordBotToken_Gpteous.txt" , 'r') as discordfile :
+with open(r"/etc/secrets/discordBotToken_Gpteous.txt" , 'r') as discordfile :
    Token_gpteousBot = discordfile.read().strip()
    
-with open(r"../bard_apiKey.txt" , 'r') as bardfile : #actually is  a coockie named : "__Secure-1PSID" from bard website coockies
+with open(r"/etc/secrets/bard_apiKey.txt" , 'r') as bardfile : #actually is  a coockie named : "__Secure-1PSID" from bard website coockies
    bardAPI_KEY = bardfile.read().strip() 
    
-with open(r"../bard_gmail.txt" , 'r') as bardfile : 
+with open(r"/etc/secrets/bard_gmail.txt" , 'r') as bardfile : 
    bardGmail = bardfile.read().strip() 
    
-with open(r"../bard_gmailPass.txt" , 'r') as bardfile : 
+with open(r"/etc/secrets/bard_gmailPass.txt" , 'r') as bardfile : 
    bardGmail_KEY = bardfile.read().strip() 
