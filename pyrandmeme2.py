922dc8b1975e269cf348b2167033dffa0e6e85f5
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-08-27 01:51:42 +0300

    # quick fix: list index out of range#

--- pyrandmeme2.py
+++ pyrandmeme2.py
@@ -10,6 +10,6 @@ async def pyrandmeme2( _title : str = "Random meme") -> discord.Embed :
     async with aiohttp.ClientSession() as cs:
         async with cs.get('https://www.reddit.com/r/memes/new.json?sort=hot') as r:
             res = await r.json()
-            pymeme.set_image(url=res['data']['children'][random2.randint(0, 25)]['data']['url'])
+            pymeme.set_image(url=res['data']['children'][random2.randint(0, 24)]['data']['url'])
             return pymeme
         await pyrandmeme2("Random meme")
