0bbc93d747eb0c7834495217b2ea70775b5e1ea5
Omar Rashad Salem <77698890+orsnaro@users.noreply.github.com>
2023-10-23 15:57:51 +0300

    # finishing the new patch `V2.5.3B` mainly added voice features + fxing schedulaed events like auto meme sender issues# remaining for this patch: track queue##

--- main_wizard_bot.py
+++ main_wizard_bot.py
@@ -1,13 +1,12 @@
 """
                           Coder : Omar
-                          Version : v2.5.2B
-                          version Date :  17 / 8 / 2023
+                          Version : v2.5.3B
+                          version Date :  23 / 10 / 2023
                           Code Type : python | Discrod | BARD | HTTP | ASYNC
                           Title : main code for Discord Bot (with generative AI)
                           Interpreter : cPython  v3.11.0 [Compiler : MSC v.1933 AMD64]
 """
 
-#TODO TODO TODO : DO THIS BEFORE any other big task -> (FINSIH THOSE & FIX BIG BUGS ) THEN => MIGRATE TO PYCORD 
 #TODO : Documentaion for functions / classes and repo readme.md
 
 #(DONE)TODO : make bot see prev messages (use session)
@@ -21,8 +20,8 @@
 #(DONE)TODO : connect to cdn that has memes / quotes  and set on_time() event to send to chat chill and ask-wizard-channel (duplicated in todo discord channel)
 #(DONE)TODO : show embedded images in bard answer (shows with the links as links :( ))
 #(DONE)TODO : make message fragmenter function for msg and links msg  and images msg in utils_bot.py
+#(DONE)TODO : replace all your manual auto meme/quote sender queue logic with this (let bot event loop handle it auto!)(no need even for acync event control var): https://discordpy.readthedocs.io/en/stable/ext/tasks/index.html#
 
-#TODO : replace all your manual auto meme/quote sender queue logic with this (les bot event loop handle it auto!)(no need even for acync event control var): https://discordpy.readthedocs.io/en/stable/ext/tasks/index.html#
 #TODO : use https://rapidapi.com/ 
 #TODO : complete your quote lib fork and make it fully async. 
 #TODO : add command : provides the ability for other SERVERS  using wizard bot to add their own '#ask-the-wizard-channel' just by  typing  `add/deleteWizardChannel` command : this command takes channel id and appends it to wizard_channels_ids tuple  ( check if you need any id for the server it self mostly not)
